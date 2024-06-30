081c704b5da9d828054bc539bc77949e85bb704b
quackandcheese <quackandcheeseart@gmail.com>
2024-06-29 22:32:28 -0500

    Revert "make SetCharacterFromNumberPatch resistant to game updates"
    This reverts commit 5a3c3a46720300349236de7c937c8cdba5db5ddb.

--- MagmaCore/Patches/SetCharacterFromNumberPatch.cs
+++ MagmaCore/Patches/SetCharacterFromNumberPatch.cs
@@ -22,18 +22,43 @@ namespace MagmaCore.Patches
                 Singleton.Instance.characterNumber = 0;
             }
 
+            if (__instance.characterNumber == 0)
+            {
+                __instance.character = Character.CharacterName.Purse;
+            }
+            if (__instance.characterNumber == 1)
+            {
+                __instance.character = Character.CharacterName.Tote;
+            }
+            if (__instance.characterNumber == 2)
+            {
+                __instance.character = Character.CharacterName.CR8;
+            }
+            if (__instance.characterNumber == 3)
+            {
+                __instance.character = Character.CharacterName.Satchel;
+            }
+            if (__instance.characterNumber == 4)
+            {
+                __instance.character = Character.CharacterName.Pochette;
+            }
+            if (CustomUtils.GetCustomsOfTypeWithID<CustomCharacter>() == null)
+            {
+                MelonLogger.Warning("No custom characters detected.");
+                return false;
+            }
             foreach (KeyValuePair<int, CustomCharacter> characterKvp in CustomUtils.GetCustomsOfTypeWithID<CustomCharacter>())//CustomCharacter.CustomCharacters)
             {
                 int paddingAmount = Resources.FindObjectsOfTypeAll<Character>().Length - CustomUtils.GetCustomsOfType<CustomCharacter>().Count;
+                MelonLogger.Msg(Resources.FindObjectsOfTypeAll<Character>().Length - CustomUtils.GetCustomsOfType<CustomCharacter>().Count);
                 if (__instance.characterNumber == CustomUtils.GetCustomsOfTypeWithID<CustomCharacter>().Values.ToList().IndexOf(characterKvp.Value) + paddingAmount)
                 {
                     MelonLogger.Warning("Custom char detected: " + characterKvp.Value.characterName);
                     __instance.character = (Character.CharacterName)characterKvp.Key;
-                    return false;
                 }
             }
 
-            return true;
+            return false;
         }
     }
 }
