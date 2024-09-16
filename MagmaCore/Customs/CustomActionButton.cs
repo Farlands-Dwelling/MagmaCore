ba3d64ee74e072dddf9cb690c39a9d0ed99d22fd
quackandcheese <quackandcheeseart@gmail.com>
2024-09-16 16:59:22 -0500

    add custom action buttons

---
+++ MagmaCore/Customs/CustomActionButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace MagmaCore.Customs
+{
+    public abstract class CustomActionButton : CustomBase
+    {
+        public Button ButtonInstance;
+        /// <value>
+        /// Use this in characters to have a custom button. Assigned automatically using casted ID.
+        /// </value>
+        public virtual ActionButtonManager.Type ButtonType { get; private set; }
+        public virtual string buttonText { get; private set; }
+        public virtual string hoverText { get; private set; }
+
+        public override void Convert()
+        {
+            if (ButtonType == default)
+            {
+                ButtonType = (ActionButtonManager.Type)GetHash();
+            }
+        }
+        public virtual void OnClick() { }
+    }
+}
---
+++ MagmaCore/Patches/CreateButtonPatch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using MagmaCore.Customs;
+using MagmaCore.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace MagmaCore.Patches
+{
+    [HarmonyPatch(typeof(ActionButtonManager), "CreateButton")]
+    class CreateButtonPatch
+    {
+        static void Postfix (ref ActionButtonManager __instance, ref GameObject __result, ActionButtonManager.Type type)
+        {
+            foreach (CustomActionButton actionButton in CustomUtils.GetCustomsOfType<CustomActionButton>())//CustomCharacter.CustomCharacters.Values)
+            {
+                if (type == actionButton.ButtonType)
+                {
+                    GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(__instance.actionButtonPrefab, Vector3.zero, Quaternion.identity, __instance.combatButtonsParent);
+                    TranslationUtils.GetOrCreateTranslation("english", actionButton.ID.ToString(), actionButton.buttonText);
+                    TranslationUtils.GetOrCreateTranslation("english", actionButton.ID.ToString() + "hover", actionButton.hoverText);
+                    gameObject.GetComponent<ReplacementText>().key = actionButton.ID.ToString();
+                    gameObject.GetComponentInChildren<SimpleHoverText>().SetText(actionButton.ID.ToString() + "hover");
+                    gameObject.GetComponent<Button>().onClick.AddListener(new UnityAction(actionButton.OnClick));
+                    __instance.numberOfCombatButtons++;
+
+                    __result = gameObject;
+                    break;
+                }
+            }
+        }
+    }
+}
